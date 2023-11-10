526297ea9ecfdc413dd416322c12271277950801
thelastenvoy <satvikbatra@gmail.com>
2023-11-10 13:45:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,36 @@ const https = require('https');
 const searchTerm = process.argv[2];
 
 function fetchData(searchTerm) {
-  // TODO: Implement the Promise-based function to fetch data from the API based on the search term
-  
+  return new Promise((resolve, reject) => {
+    const url = `https://jsonplaceholder.typicode.com/todos/${searchTerm}`;
+
+    https.get(url, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        if (res.statusCode === 200) {
+          resolve(JSON.parse(data));
+        } else {
+          reject(`API request failed with status code ${res.statusCode}`);
+        }
+      });
+
+      res.on('error', (error) => {
+        reject(`Error fetching data from API: ${error.message}`);
+      });
+    });
+  });
 }
 
 fetchData(searchTerm)
   .then((data) => {
-    // TODO: Process the retrieved data
+    console.log(data);
   })
   .catch((error) => {
-    // TODO: Handle any errors that occur during the API request
+    console.error(error);
   });
+
+module.exports = { fetchData }; // Export for testing purposes
